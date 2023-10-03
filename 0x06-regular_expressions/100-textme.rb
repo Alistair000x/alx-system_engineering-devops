d1cd908a3855d32b1a30a78cf14fbc1367cb1de3
alistair000x <darmensun43@gmail.com>
2023-10-03 11:16:08 -0700

    yes

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <log_file>"
-  exit(1)
-end
-
-log_file = ARGV[0]
-log_data = File.read(log_file)
-pattern = /\[from:(?<sender>[^\]]+)\] \[to:(?<receiver>[^\]]+)\] \[flags:(?<flags>[^\]]+)\]/
-matches = log_data.scan(pattern)
-
-matches.each do |match|
-  sender = match[0].strip
-  receiver = match[1].strip
-  flags = match[2].strip
-  puts "#{sender},#{receiver},#{flags}"
-end
+puts ARGV[0].scan(/\[from:([^\[\]]+)\].\[to:([^\[\]]+)\].\[flags:([^\[\]]+)\]/).join(',')
